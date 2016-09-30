d4789c888ce83dbd4493b292135549e7fb633f9f
rptorres22 <rtorres22@gmail.com>
2016-09-30 15:27:08 -0700

    added authentication and db

--- saas-tutorial/server/config/main.js
+++ saas-tutorial/server/config/main.js
@@ -1,3 +1,8 @@
 module.exports = {
+  // Secret key for JWT signing and encryption
+  'secret': 'super secret passphrase',
+  // Database connection information
+  'database': 'mongodb://localhost:27017/saas-tutorial',
+  // Setteing port for server
   'port': process.env.PORT || 3000
 }
