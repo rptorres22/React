40d464e40ad70158578a5f353f0c9ee115aca56f
rptorres22 <rtorres22@gmail.com>
2016-09-30 08:51:48 -0700

    new saas project

---
+++ saas-tutorial/server/index.js
@@ -0,0 +1,7 @@
+// Importing Node modules and initializing Express
+const express = require('express'),
+      app = express(),
+      logger = require('morgan'),
+      config = require('./config/main');
+
+      
