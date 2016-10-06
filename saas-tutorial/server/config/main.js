6cf2645c376018f1a74165e202dd2637ae7f5a77
rptorres22 <rtorres22@gmail.com>
2016-10-06 15:59:29 -0700

    added chat feature

--- saas-tutorial/server/config/main.js
+++ saas-tutorial/server/config/main.js
@@ -5,4 +5,12 @@ module.exports = {
   'database': 'mongodb://localhost:27017/saas-tutorial',
   // Setteing port for server
   'port': process.env.PORT || 3000
+  // Configuration Mailgun API for sending transactional email
+  //'mailgun_priv_key': 'mailgun private key here',
+  // Configuration Mailgun domain for sending transactional email
+  //'mailgun_domain': 'mailgun domain here',
+  // Mailchimp API key
+  //'mailchimpApiKey': 'mailchimp api key here',
+  // Stripe API key'
+  //'stripeApiKey': 'stripe api key goes here'
 }
--- saas-tutorial/server/router.js
+++ saas-tutorial/server/router.js
@@ -1,4 +1,8 @@
 const AuthenticationController = require('./controllers/authentication'),
+      UserController = require('./controllers/user'),
+      ChatController = require('./controllers/chat'),
+      //CommunicationController = require('./controllers/communication'),
+      //StripeController = require('./controllers/stripe'),
       express = require('express'),
       passportService = require('./config/passport'),
       passport = require('passport');
@@ -17,7 +21,11 @@ const REQUIRE_ADMIN = "Admin",
 module.exports = function(app) {
   // Initializing route groups
   const apiRoutes = express.Router(),
-        authRoutes = express.Router();
+        authRoutes = express.Router(),
+        userRoutes = express.Router(),
+        chatRoutes = express.Router(),
+        payRoutes = express.Router(),
+        communicationRoutes = express.Router();
 
   //=========================
   // Auth Routes
@@ -31,6 +39,76 @@ module.exports = function(app) {
 
   // Login route
   authRoutes.post('/login', requireLogin, AuthenticationController.login);
+
+  // Password reset request route (generate/send token)
+  authRoutes.post('/forgot-password', AuthenticationController.forgotPassword);
+  authRoutes.post('/reset-password/:token', AuthenticationController.verifyToken);
+
+  //=========================
+  // User Routes
+  //=========================
+
+  // Set user routes as a subgroup/middleware to apiRoutes
+  apiRoutes.use('/user', userRoutes);
+
+  // View user propfile route
+  userRoutes.get('/:userId', requireAuth, UserController.viewProfile);
+
+  // Test protected route
+  apiRoutes.get('/protected', requireAuth, function(req, res) {
+    res.send({ content: 'The protected test route is functional!'});
+  });
+
+  //=========================
+  // Chat Routes
+  //=========================
+
+  // Set chat routes as a subgroup/middleware to apiRoutes
+  apiRoutes.use('/chat', chatRoutes);
+
+  // View messages to and from authenticated user
+  chatRoutes.get('/', requireAuth, ChatController.getConversations);
+
+  // Retrieve single conversation
+  chatRoutes.get('/:conversationId', requireAuth, ChatController.getConversation);
+
+  // Send reply in conversation
+  chatRoutes.post('/:conversationId', requireAuth, ChatController.sendReply);
+
+  // Start new conversation
+  chatRoutes.post('/new/:recipient', requireAuth, ChatController.newConversation);
+
+  //=========================
+  // Payment Routes
+  //=========================
+  //apiRoutes.use('/pay', payRoutes);
+
+  // Webhook endpoint for Stripe
+  //payRoutes.post('/webhook-notify', StripeController.webhook);
+
+  // Create customer and subscription
+  //payRoutes.post('/customer', requireAuth, StripeController.createSubscription);
+
+  // Update customer object and billing information
+  //payRoutes.put('/customer', requireAuth, StripeController.updateCustomerBillingInfo);
+
+  // Delete subscription from customer
+  //payRoutes.delete('/subscription', requireAuth, StripeController.chargeSubscription);
+
+  // Upgrade or dowdngrade subscription
+  //payRoutes.put('/subscription', requireAuth, StripeController.changeSubscription);
+
+  // Fetch customer information
+  //payRoutes.get('/customer', requireAuth, StripeController.getCustomer);
+
+  //=========================
+  // Communication Routes
+  //=========================
+
+  //apiRoutes.use('/Communication', communicationRoutes);
+
+  // Send email from contact form
+  //communicationRoutes.post('/contact', CommunicationController.sendContactForm);
 
   // Set url for API group routes
   app.use('/api', apiRoutes);
